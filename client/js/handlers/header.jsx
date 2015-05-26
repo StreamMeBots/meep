dacf6cf17d2e8bc83de412f165bb80fe210f00de
Jordan <jordan@vubeology.com>
2015-05-26 09:59:58 -0500

    Minor style changes

--- client/js/handlers/header.jsx
+++ client/js/handlers/header.jsx
@@ -6,7 +6,27 @@ module.exports = function(el) {
 	var menu = '';
 
 	if(user.isAuthenticated()) {
-		menu = (<ul><li><a href='/logout' title='Sign out'>Sign Out</a></li></ul>);
+		var avatarStyle = {
+			background: 'url(' + user.get('avatar') + ')',
+			'background-size': 'cover'
+		};
+
+		var channelLink = 'https://www.stream.me/' + user.get('username');
+
+		menu = (
+			<ul>
+				<li>
+					<span className='welcome'>
+						<a target='_blank' href={channelLink} title={user.get('username')}>
+							<span style={avatarStyle} className='tinyAvatar' />
+							{ user.get('username') }
+						</a>
+					</span>
+
+					<a href='/logout' title='Sign out'>Sign Out</a>
+				</li>
+			</ul>
+		);
 	}
 
 	React.render(
